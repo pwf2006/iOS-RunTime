fcf249283bc858eb21ffd5d023eb7bec8b929b1c
pwf2006 <674423263@qq.com>
2016-08-03 14:21:41 +0800

    增加一个label
    主界面增加一个label

--- FirstGit/HomeController.m
+++ FirstGit/HomeController.m
@@ -8,11 +8,30 @@
 
 #import "HomeController.h"
 
+@interface HomeController ()
+
+@property (nonatomic, strong) UILabel *msgLbl;
+
+@end
+
 @implementation HomeController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.msgLbl = [[UILabel alloc] init];
+    self.msgLbl.font = [UIFont systemFontOfSize:14];
+    self.msgLbl.textColor = [UIColor blackColor];
+    self.msgLbl.text = @"主界面";
+    self.msgLbl.textAlignment = NSTextAlignmentLeft;
+    [self.view addSubview:self.msgLbl];
+    
     self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    self.msgLbl.frame = CGRectMake(20, 40, 60, 20);
 }
 @end
