bd9f966e7486da4f105ac277341af01a5d7cc794
pwf2006 <674423263@qq.com>
2016-08-03 14:11:09 +0800

    工程初始化

---
+++ FirstGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstGit
+//
+//  Created by pengweifeng on 8/3/16.
+//  Copyright © 2016 pengweifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstGit/HomeController.m
@@ -0,0 +1,18 @@
+//
+//  HomeController.m
+//  FirstGit
+//
+//  Created by pengweifeng on 8/3/16.
+//  Copyright © 2016 pengweifeng. All rights reserved.
+//
+
+#import "HomeController.h"
+
+@implementation HomeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+@end
