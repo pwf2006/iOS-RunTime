5c8064e4349861a2acfd796b77f7424e73ed6924
wangmo <wangmo@eastmoney.com>
2017-07-05 17:43:46 +0800

    解决冲突

--- FirstGit/AppDelegate.m
+++ FirstGit/AppDelegate.m
@@ -18,7 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
+
     HomeController *homeVC = [HomeController new];
     self.window.rootViewController = homeVC;
     self.window.backgroundColor = [UIColor whiteColor];
